7ba231c9eabd302660115c385563e0c6a8e7a365
RyanQuey <rlquey2@Gmail.com>
2017-08-13 22:35:23 -0700

    begin configuring some stuff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import admin from 'firebase-admin'
+import serviceAccount from './serviceAccountKey.json'
+
+// initialize firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://growth-tools-70f94.firebaseio.com"
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
