b38a04273303d57b00f8d5436a92f00d31bf3732
RyanQuey <rlquey2@Gmail.com>
2017-09-18 00:36:00 -0700

    remove schema comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -34,77 +34,12 @@ sagaMiddleware.run(rootSaga)
 export default window.store
 
 /* SCHEMA
- *
- *******************************
- * channelConfiguration: {
- *   id
- *   planId: (belongs to one plan)
- *   name: "PERSONAL_POST",
- *   provider: "FACEBOOK",
- *   utmDefaults: {
- *     custom: false or undefined if not, a string if yes
- *     enabled: Boolean
- *     medium: (use more constants...such as CHANNEL_NAME, PLAN_NAME, URL, )
- *     source:
- *     content:
- *     term:
- *   }  
- * },
- *******************************
- * messages
- *   id
- *   post: [postId]
- *   channel: can only have one channel( 
- *   utm: {
- *     custom: false or undefined if not, a string if yes
- *     enabled: Boolean
- *     medium: 
- *     source:
- *     content:
- *     term:
- *   },
- *   thumbnail: [string for data storage]
- *******************************
- * plans
- *   id
- *   name
- *   userId
- *   posts: [postId, postId, ...]
- *   channels: [array of channel IDs]
- *   providers: [array of string constantss of provider names] (necessary to toggle entire providers without messing up channel configurations)
- *   createdAt     
- *   updatedAt
- *******************************
- * posts
- *   id
- *   userId?? (perhaps could just use relation through the planId? depends on fire bases querying method)
- *   messages: [array of message IDs]
- *   planId: [planId]
- *   createdAt 
- *   status: ("published", "draft", "archived")    
- *
- * 
- *******************************
- * users
- *   id
- *   email: required
- *
- *   firstName
- *   lastName
- *   plans
- *******************************
- * userProviders
- *   id
- *   providerName
- *   channels: {
- *     name: ("PERSONAL_STATUS_UPDATE", etc),
- *     type: "group",
- *     lastPost (?)
- *   }
- *   token?? 
- *   email:
- *   displayName:
- *   profilePicture:
- *
+ provider
+ channelConfiguration:
+ messages
+ plans
+ posts
+ users
+
  */
 
