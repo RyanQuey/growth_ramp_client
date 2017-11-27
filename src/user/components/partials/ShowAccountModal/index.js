98d1f267644fe19b130b5049c743fe0d397086cd
RyanQuey <rlquey2@Gmail.com>
2017-11-26 23:14:07 -0800

    squash this

---
+++ src/user/components/partials/ShowAccountModal/index.js
@@ -0,0 +1,170 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {
+  ModalContainer,
+  ModalBody,
+  ModalFooter,
+} from 'shared/components/partials/Modal'
+import { CLOSE_MODAL, LINK_ACCOUNT_REQUEST, REFRESH_CHANNEL_TYPE_REQUEST } from 'constants/actionTypes'
+import { SocialLogin } from 'shared/components/partials'
+import { PermissionRow } from 'user/components/groups'
+import { AccountCard } from 'user/components/partials'
+import { Button, Form, Card, Flexbox } from 'shared/components/elements'
+import { ButtonGroup } from 'shared/components/groups'
+import { PROVIDERS } from 'constants/providers'
+import classes from './style.scss'
+
+
+class ShowAccount extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loginPending: false,
+      mode: 'CHOOSE_PROVIDER', //you do this or 'CHOOSE_SCOPE'
+      channelTypes: [],
+    }
+
+    this.onSuccess = this.onSuccess.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.setPending = this.setPending.bind(this)
+    this.chosenScopes = this.chosenScopes.bind(this)
+    this.chooseChannel = this.chooseChannel.bind(this)
+    this.refreshChannelType = this.refreshChannelType.bind(this)
+  }
+
+  handleClose (){
+    this.props.closeModal()
+  }
+
+  chooseChannel(channelType) {
+    this.setState({channelType})
+  }
+
+  onSuccess () {
+    this.handleClose();
+
+    if (typeof this.props.onSuccess === 'function') {
+      this.props.onSuccess();
+    }
+  }
+  setPending() {
+    this.setState({loginPending: true})
+  }
+
+  chosenScopes() {
+    //might make a helper function if I needed anywhere else
+    //takes channelTypes and provider and returns the scopes needed for that channel
+    const scopes = []
+    const provider = this.state.currentProvider
+
+    this.state.channelTypes.forEach((channel) => {
+      const newScopes = PROVIDERS[provider].channelTypes[channel].filter((scope) => !scopes.includes(scope))
+      scopes.push(...newScopes)
+    })
+
+    //removing duplicates
+    return scopes
+  }
+
+  refreshChannelType(channelType) {
+    this.setState({pending: true})
+    const cb = () => {
+      this.setState({pending: false})
+    }
+
+    this.props.refreshChannelType(channelType, this.props.currentAccount, cb)
+  }
+
+  render (){
+    //doesn't allow things to break and actually better performance
+    const visible = this.props.currentModal === "ShowAccountModal"
+    if (!visible) {
+      return null
+    }
+
+    const account = this.props.currentAccount
+    const provider = account.provider
+    const providerName = PROVIDERS[provider].name
+//might combine with potentialChannels somehow, or channelTypes
+    const permittedChannelTypes = Helpers.permittedChannels(account)
+
+    const channels = account.channels
+
+    return (
+      <ModalContainer
+        visible={visible}
+        onClose={this.handleClose}
+        title={`${account.userName || account.email}'s ${providerName} account`}
+        headerImgUrl={account.photoUrl}
+      >
+        <ModalBody>
+
+          <div>
+
+            <div>
+              <h3>Current Channel Types:</h3>
+              {permittedChannelTypes.length ? (
+                <div>
+                  {permittedChannelTypes.map((channelType) => {
+                    const channelName = PROVIDERS[provider].channelTypes[channelType].name
+
+                    return <div key={channelType}>
+                      <h4>{channelName}</h4>
+                      {channels.filter((c) => c.type === channelType).map((c) => <div>{c.id}</div>)}
+                      {channelType !== "PERSONAL_POST" &&
+                        <Button onClick={this.refreshChannelType.bind(this, channelType)}>
+                          Refresh {channelName} channels from {providerName}
+                        </Button>
+                      }
+                    </div>
+                  })}
+                </div>
+              ) : (
+                <div>No permitted channels yet</div>
+              )}
+            </div>
+
+            <Flexbox justify="center">
+              {Object.keys(PROVIDERS[provider].channelTypes).map((channel) =>
+                <Button
+                  key={channel}
+                  style="inverted"
+                  onClick={this.chooseChannel.bind(this, channel)}
+                  selected={this.state.channelTypes.includes(channel)}
+                >
+                  {channel.titleCase()}
+                </Button>
+              )}
+            </Flexbox>
+          </div>
+        </ModalBody>
+      </ModalContainer>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    closeModal: (payload) => dispatch({type: CLOSE_MODAL, payload}),
+    linkAccountRequest: (payload) => dispatch({type: LINK_ACCOUNT_REQUEST, payload}),
+    refreshChannelType: (channelType, account, cb) => dispatch({
+      type: REFRESH_CHANNEL_TYPE_REQUEST,
+      payload: {
+        channelType,
+        account,
+      },
+      cb,
+    }),
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    currentModal: state.viewSettings.currentModal,
+    options: state.viewSettings.modalOptions || {},
+    currentAccount: Helpers.safeDataPath(state.viewSettings, "modalOptions.currentAccount", {}),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowAccount)
