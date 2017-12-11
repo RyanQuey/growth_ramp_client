b503529e21244a6a643965f950adf4ace8912d8f
RyanQuey <rlquey2@Gmail.com>
2017-12-11 15:47:32 -0800

    add files

---
+++ src/helpers/campaignHelpers.js
@@ -0,0 +1,120 @@
+export default {
+  parseCampaignResults: (data) => {
+    const posts = data.posts
+    let ret = {
+      //campaign and posts will be set to the store no matter what, as is
+      campaign: data,
+      posts: posts,
+      //this is for all
+      failedPosts: [],
+      //note: these also failed to publish, but failed even at the short-link phase
+      //putting these posts in both
+      failedToShortenUrl: [],
+      failedToPublish: [],
+      requireReauthorization: [], //the posts
+      accountsToReauthorize: [], //the accounts
+      //most dangerous; published, but our db isn't reflecting it
+      publishedButNotUpdated: [],
+
+      alertMessage: "",  //for the alert
+      alertTitle: "Successfully Published Posts!",      //
+      alertLevel: "SUCCESS",
+      alertTimer: true, //default to this, unless there are errors
+    }
+
+
+    for (let post of posts) {
+      //if has error property, something went wrong somewhere along the way.
+      if (post.error) {
+        ret.failedPosts.push(post)
+        ret.alertTimer = false
+        ret.alertLevel = "DANGER"
+
+        //parse the code that GR api set
+        switch (post.error.code) {
+          case "no-post":
+            ret.failedToShortenUrl.push(post)
+            ret.failedToPublish.push(post)
+            break
+          //don't need to know what shortenedURl already or not; but we're ready for if we do
+          case "failed-to-shorten-link":
+            ret.failedToShortenUrl.push(post)
+            ret.failedToPublish.push(post)
+            break
+
+          case "failed-to-publish-unknown":
+            ret.failedToPublish.push(post)
+            break
+
+          case "require-reauthorization":
+            ret.failedToPublish.push(post)
+            ret.requireReauthorization.push(post) //the posts
+            break
+
+          //this is scope issue; but still prompting same thing for now
+          case "insufficient-permissions":
+            ret.failedToPublish.push(post)
+            ret.requireReauthorization.push(post) //the posts
+            break
+
+          case "unknown-error-while-publishing":
+            ret.failedToPublish.push(post)
+            break
+
+          case "rate-limit-reached":
+            ret.failedToPublish.push(post)
+            break
+
+          case "duplicate-post":
+            ret.failedToPublish.push(post)
+            break
+
+          case "published-but-failed-to-save":
+            ret.publishedButNotUpdated.push(post)
+            break
+          default:
+
+        }
+      }
+    }
+
+    //extract accounts requiring reauthorization
+    if (ret.requireReauthorization.length) {
+
+      for (let post of ret.requireReauthorization) {
+        let accountId = post.providerAccountId
+        if (!ret.accountsToReauthorize.includes(accountId)) {
+          ret.accountsToReauthorize.push(accountId)
+        }
+      }
+
+      //now array of records
+      const allProviderAccounts = Helpers.flattenProviderAccounts()
+console.log(allProviderAccounts);
+      ret.accountsToReauthorize = ret.accountsToReauthorize.map((id) => allProviderAccounts.find((account) => account.id === id))
+    }
+
+    if (ret.failedPosts.length ) {
+
+      if (ret.failedPosts.length === posts.length ) {
+        ret.alertTitle = "All posts failed to publish:"
+      } else {
+        ret.alertTitle = "Some posts failed to publish:"
+      }
+
+      let messages = []
+      if (ret.accountsToReauthorize.length) {
+console.log(ret.accountsToReauthorize);
+        let accountNames = ret.accountsToReauthorize.map((account) => `${Helpers.providerFriendlyName(account.provider)} (${account.userName || account.email})`)
+
+        messages.push(`Please login to ${accountNames.join("; ")}`)
+      }
+
+      ret.alertMessage = messages.join("; ")
+      ret.alertMessage += ". Then try publishing again."
+    }
+
+console.log("Full campaign results:", ret);
+    return ret
+  },
+}
---
+++ src/helpers/index.js
@@ -0,0 +1,123 @@
+import uuid from 'uuid/v1';
+//TODO remove lib if possible...but other libs might require it or something
+import $ from 'jquery';
+import {
+  CLEAR_ERRORS,
+  HANDLE_ERRORS,
+} from 'constants/actionTypes'
+import {
+  errorActions
+} from 'shared/actions'
+import {
+  newAlert
+} from 'shared/actions/alerts'
+import { PROVIDERS } from 'constants/providers'
+import campaignHelpers from './campaignHelpers'
+
+let Helpers = {
+  // extracts the relevant passport profile data from the profile auth data received on login/request, and matches it to the database columns
+  // don't think I ever use this...only in nodeHelpers.js
+  safeDataPath: function (object, keyString, def = null) {
+    let keys = keyString.split('.');
+    let returnValue = def;
+    let safeObject = object;
+
+    if (!safeObject) {
+      return def;
+    }
+
+    for (let key of keys) {
+      if (safeObject[key]) {
+        returnValue = safeObject[key];
+        safeObject = safeObject[key];
+      } else {
+        return def;
+      }
+    }
+
+    return returnValue;
+  },
+
+  uniqueId: function () {
+    return uuid()
+  },
+
+  //might do away with...but might be helpful if have lots of fields to iterate over
+  handleParam: function (e, key) {
+    const objKey = key || e.target.dataset.key
+    const obj = {};
+
+    obj[objKey] = e.target.value;
+
+    this.setState(obj);
+  },
+
+  //TODO don't use this anymore, just call error actions
+  notifyOfAPIError: (errors, templateName, templatePart, options = {})  => {
+    console.log("DEPRECATED_._DON'T_USE_THIS");
+    console.log(errors);
+    errorActions.handleErrors(errors, templateName, templatePart, options)
+  },
+
+  //takes a list of scopes for an account and returns the list of available channels for that account
+  //might make a helper function if I needed anywhere else
+  permittedChannelTypes: (account) => {
+    if (!account || typeof account !== "object") {
+      return []
+    }
+    const permittedScopes = Object.keys(account.scopes).filter((scopeType) => {
+      return account.scopes[scopeType].status === 'granted'
+    })
+
+    const permittedChannelTypes = Object.keys(PROVIDERS[account.provider].channelTypes).filter((channelType) => {
+      const channelScopes = PROVIDERS[account.provider].channelTypes[channelType].requiredScopes
+
+      return channelScopes.every((scope) => (permittedScopes.includes(scope))) //also returns true when the channel requires no scopes at all (empty array)
+    })
+
+    return permittedChannelTypes
+  },
+
+  accountFromPost: (post) => {
+    const providerAccounts = store.getState().providerAccounts[post.provider]
+    const postAccount = providerAccounts.find((account) => account.id === (post.providerAccountId.id || post.providerAccountId))
+    return postAccount
+  },
+
+  //only for when channel type has multiple
+  channelFromPost: (post) => {
+    const postAccount = Helpers.accountFromPost(post)
+    const channelRecord = postAccount.channels.find(channel => channel.id === (post.channelId.id || post.channelId))
+    return channelRecord
+  },
+
+  //takes upper scored provider name and returns friendly name
+  //either need channel or the other two
+  providerFriendlyName: (providerName) => PROVIDERS[providerName].name,
+
+  //takes channel record and returns friendly name
+  //either need channel or the other two
+  channelTypeFriendlyName: (channel, providerName, channelType) => PROVIDERS[providerName || channel.provider].channelTypes[channelType || channel.type].name,
+
+  //takes channel record and returns whether the channel type normally has multiple channels for it
+  //either need channel or the other two
+  //NOTE currently, ALL channel records have multiple, hence why they are channel records. Personal posts don't get a record.
+  channelTypeHasMultiple: (channel, providerName, channelType) => PROVIDERS[providerName || channel.provider].channelTypes[channelType || channel.type].hasMultiple,
+
+  //takes channel record and returns required scopes
+  //either need channel or the other two
+  channelTypeScopes: (channel, providerName, channelType) => PROVIDERS[providerName || channel.provider].channelTypes[channelType || channel.type].requiredScopes,
+
+  channelPostingAsTypes: (channel, providerName, channelType) => PROVIDERS[providerName || channel.provider].channelTypes[channelType || channel.type].postingAsTypes,
+
+  //flattening to a single array
+  flattenProviderAccounts: () => {
+    let allProviderAccounts = _.values(Helpers.safeDataPath(store.getState(), `providerAccounts`, {}))
+    allProviderAccounts = [].concat.apply([], allProviderAccounts)
+    return allProviderAccounts
+  },
+}
+
+Helpers = Object.assign(Helpers, campaignHelpers)
+
+export default Helpers
