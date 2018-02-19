e0862a52f89fe4890be1803af3b069258e27e3de
RyanQuey <rlquey2@Gmail.com>
2018-02-18 21:17:56 -0800

    quick fix

--- src/constants/analytics.js
+++ src/constants/analytics.js
@@ -3,6 +3,7 @@ export const ANALYTICS = ""
 export const DIMENSIONS_METRICS_FRIENDLY_NAME = {
   //dimensions
   "ga:landingPagePath": "Landing Page",
+  "ga:channelGrouping": "Default Channel Grouping",
   //metrics
   "ga:pageviews": "Page Views",
   "ga:uniquePageviews": "Unique Page Views",
