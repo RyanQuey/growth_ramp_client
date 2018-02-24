51d3fc356b9330ddf9885db609a9be3e098e83e1
RyanQuey <rlquey2@Gmail.com>
2018-02-24 08:13:50 -0800

    quickfix

--- src/helpers/analyticsHelpers.js
+++ src/helpers/analyticsHelpers.js
@@ -17,12 +17,9 @@ export default {
       unit = "Week"
       step = 1
 
-    } else if (filterLength < 30 * 30) {
+    } else { //if (filterLength < 30 * 30) {
+      //GA doesn't increment by nthYears, so just do months
       unit = "Month"
-      step = 1
-
-    } else {
-      unit = "Year" //TODO not entirely accurate, because doesn't do beginning of year until end of year logic in GA for years. Need to change to just display sample times along the bottom
       step = 1
     }
 
@@ -38,9 +35,7 @@ export default {
     unit = unit.toLowerCase()
 
     let formatString
-    if (unit === "year") {
-      formatString = "YYYY"
-    } else if (unit === "month") {
+    if (unit === "month") {
       formatString = "MMM 'YY"
     } else if (unit === "week") {
       formatString = "ddd M/D "
