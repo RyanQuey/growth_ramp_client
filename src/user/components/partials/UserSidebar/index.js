5c756aa5f8a3df766318d65047f7791b2cf173d1
RyanQuey <rlquey2@Gmail.com>
2018-03-08 00:01:00 -0800

    add sidebar back in

--- src/user/components/partials/UserSidebar/index.js
+++ src/user/components/partials/UserSidebar/index.js
@@ -59,12 +59,12 @@ class UserSidebar extends Component {
               </ul>
             </MenuItem>
 
-            {false && <MenuItem link={false && "/analytics"} text="Traffic Dashboard" selected={this.state.trafficDashboard} onClick={this.handleClick.bind(this, "trafficDashboard")} icon="bar-chart">
+            <MenuItem link={false && "/analytics"} text="Traffic Dashboard" selected={this.state.trafficDashboard} onClick={this.handleClick.bind(this, "trafficDashboard")} icon="bar-chart">
               <ul>
                 <MenuChild text="Website Overview" link={`/analytics/website-overview`} nav={true} icon={""}/>
                 <MenuChild text="Landing Pages" link={`/analytics/landing-pages`} nav={true} icon={""}/>
               </ul>
-            </MenuItem>}
+            </MenuItem>
             {false && <MenuItem link="/workgroups" text="Workgroups" nav={true} icon="users"/>}
 
           </ul>
