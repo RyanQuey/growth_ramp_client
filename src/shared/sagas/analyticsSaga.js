e469b60007152bce98ae9dbd8622885a2f68e317
RyanQuey <rlquey2@Gmail.com>
2018-05-04 21:19:11 -0700

    if not paid yet

--- src/shared/sagas/analyticsSaga.js
+++ src/shared/sagas/analyticsSaga.js
@@ -43,7 +43,7 @@ function* fetchAllGAAccounts(action) {
 
   } catch (err) {
     console.error('all GA accounts fetch failed', err.response || err)
-    if (Helpers.safeDataPath(err, "response.data.message") === "User does not have any Google Analytics account.") {
+    if (Helpers.safeDataPath(err, "data.message") === "User does not have any Google Analytics account.") {
       err.message = "User does not have any Google Analytics account."
     }
       alertActions.newAlert({
